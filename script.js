69fbf4c471ccae7d6ad31070d8de3aac2cfa56a2
Nischay <nischaymaharjann@gmail.com>
2023-08-26 21:36:35 +0545

    Completed

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 const tableContent = document.querySelector(".table-content");
-
+const searchValue = document.querySelector("#search-bar");
 let pokemonsArr;
+let rawPokemons;
+let b;
+let a;
+let ascendingSort = document.querySelector(".ascending");
+let descendingSort = document.querySelector(".descending");
 
 const getPokemons = async () => {
   let url = "https://pokeapi.co/api/v2/pokemon?&limit=50";
   const response = await fetch(url);
   const value = await response.json();
-  pokemonsArr = value.results
-  fetchPokemons(pokemonsArr, deleteFunc)
-}
+  pokemonsArr = value.results;
+  searchValue.value = "";
+  fetchPokemons(pokemonsArr);
+  console.log(pokemonsArr);
+
+  // Delete Function
+  a = document.querySelectorAll(".trash-button");
+  console.log(a);
+  a.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let element = btn.parentElement.parentElement;
+      element.remove();
+      console.log(pokemonsArr);
+      fetchPokemons(pokemonsArr);
+    });
+  });
+
+  //AscendingOrder
+  ascendingSort.addEventListener("click", sortA);
+
+  //DESCENDINGSORT
+  descendingSort.addEventListener("click", sortB);
+};
+
 getPokemons();
 
-const deleteFunc = (id) => {
-  pokemonsArr.splice(id, 1)
-  tableContent.innerHTML = " "
-  fetchPokemons(pokemonsArr)
-}
+// const deleteFunc = (id) => {
+//   rawPokemons.splice(id, 1);
+//   tableContent.innerHTML = " ";
+//   if (searchValue == "") {
+//     fetchPokemons(pokemonsArr);
+//   } else {
+//     searchFun();
+//   }
+// };
 
 const fetchPokemons = (pokemons) => {
   pokemons.forEach((poke, i) => {
@@ -23,8 +53,7 @@ const fetchPokemons = (pokemons) => {
       .slice(34, 36)
       .replace(/[^0-9]/g, "")}.png`;
 
-    tableContent.innerHTML +=
-      `<tr class="row">
+    tableContent.innerHTML += `<tr class="row">
       <td class="id">${i + 1}</td>
 
       <td class="Poke-name-img">
@@ -35,10 +64,66 @@ const fetchPokemons = (pokemons) => {
       <td class="url">${poke.url}</td>
 
       <td>
-          <button class="trash-button" onclick="deleteFunc(${i})" id="trash">
+          <button class="trash-button" id="trash">
             <img src="images/icons8-trash-50.png" alt="" class="trash-img"/>
           </button>
       </td>
     </tr>`;
   });
-}
+};
+
+const searchFun = () => {
+  console.log(pokemonsArr);
+  b = pokemonsArr.filter((n) => {
+    searchinput = searchValue.value.toLowerCase();
+    return n.name.startsWith(searchinput);
+  });
+  console.log(b);
+  tableContent.innerHTML = " ";
+  b.forEach(() => {
+    tableContent.innerHTML = " ";
+    fetchPokemons(b);
+  });
+  a = document.querySelectorAll(".trash-button");
+  console.log(a);
+  a.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let element = btn.parentElement.parentElement;
+      element.remove();
+    });
+  });
+};
+
+const sortA = () => {
+  tableContent.innerHTML = " ";
+  const sortFun = (a, b) => {
+    return a.name < b.name ? -1 : 1;
+  };
+  pokemonsArr.sort(sortFun);
+  fetchPokemons(pokemonsArr);
+  a = document.querySelectorAll(".trash-button");
+  console.log(a);
+  a.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let element = btn.parentElement.parentElement;
+      element.remove();
+    });
+  });
+};
+
+const sortB = () => {
+  tableContent.innerHTML = " ";
+  const sortFun = (a, b) => {
+    return a.name < b.name ? 1 : -1;
+  };
+  pokemonsArr.sort(sortFun);
+  fetchPokemons(pokemonsArr);
+  a = document.querySelectorAll(".trash-button");
+  console.log(a);
+  a.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let element = btn.parentElement.parentElement;
+      element.remove();
+    });
+  });
+};
